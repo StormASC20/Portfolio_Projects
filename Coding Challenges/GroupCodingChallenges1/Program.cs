614ba7d1fe8fbcf2c8273c89aa8bb97a0e1de7c0
Mackenna Roberts <mlr4875@rit.edu>
2023-01-20 14:21:50 -0500

    moved my comment

--- Coding Challenges/GroupCodingChallenges1/Program.cs
+++ Coding Challenges/GroupCodingChallenges1/Program.cs
@@ -1,14 +1,13 @@
 ﻿// Mackenna, David, Storm, and Ben
 // 1/20/2023
 // Practice with merging files within a shared repo
-
+//im the shabble wobble eeble dobble weeble dobble dee
 // This is Ben's comment
 
 namespace GroupCodingChallenges1
 {
     internal class Program
     {
-        //im the shabble wobble eeble dobble weeble dobble dee
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
