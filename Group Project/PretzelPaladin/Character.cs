3f942519aeeb0fe5908bc47af59cf748b914833c
Storm Rivera <sxr7411@rit.edu>
2023-03-06 13:46:54 -0500

    commit

--- Group Project/PretzelPaladin/Character.cs
+++ Group Project/PretzelPaladin/Character.cs
@@ -4,25 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace PretzelPaladin
 {
-    internal class Character 
+    internal class Character
     {
-        private string name;
-        private int maxHealth;
-        private int currentHealth;
+        string name;
+        int maxHealth;
+        int currentHealth;
         //move array here
 
-        public Character(string name, int maxHealth, int currentHealth)        {
+        public Character(string name, int maxHealth, int currentHealth)
+        {
             this.name = name;
             this.maxHealth = maxHealth;
             this.currentHealth = currentHealth;
         }
-        
+
         public string Name
         {
             get { return name; }
-            set { name = value; }
         }
 
         public int MaxHealth
@@ -36,7 +42,6 @@ namespace PretzelPaladin
             get { return currentHealth; }
             set { currentHealth = value; }
         }
-
 
     }
 }
