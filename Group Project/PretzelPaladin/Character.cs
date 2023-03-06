1f47158927c39065a80b991a6d6fb025f363fe2b
David Shaffer <dms8826@rit.edu>
2023-03-06 13:37:50 -0500

    Interface for moves

--- Group Project/PretzelPaladin/Character.cs
+++ Group Project/PretzelPaladin/Character.cs
@@ -6,19 +6,20 @@ using System.Threading.Tasks;
 
 namespace PretzelPaladin
 {
-    internal class Character 
+    internal class Character
     {
         string name;
         int maxHealth;
         int currentHealth;
         //move array here
 
-        public Character(string name, int maxHealth, int currentHealthnam)        {
+        public Character(string name, int maxHealth, int currentHealth)
+        {
             this.name = name;
             this.maxHealth = maxHealth;
             this.currentHealth = currentHealth;
         }
-        
+
         public string Name
         {
             get { return name; }
