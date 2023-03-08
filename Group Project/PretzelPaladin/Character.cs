e870d10fefbd8b1f6597e7211774f1c746185f6b
David Shaffer <dms8826@rit.edu>
2023-03-08 14:29:54 -0500

    Pulling current updated code

--- Group Project/PretzelPaladin/Character.cs
+++ Group Project/PretzelPaladin/Character.cs
@@ -8,7 +8,7 @@ namespace PretzelPaladin
     {
         // Fields --
         private string name;
-        private int maxHealth;
+        private int maxHealth; 
         private int currentHealth;
         private int attackMultiplier;
         private int defenseMultiplier;
