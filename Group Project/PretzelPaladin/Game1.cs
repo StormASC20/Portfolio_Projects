ed928361c5566c5bc012b0c628c3069ad687eda1
David Shaffer <dms8826@rit.edu>
2023-03-29 14:37:44 -0400

    Getting updated code

--- Group Project/PretzelPaladin/Game1.cs
+++ Group Project/PretzelPaladin/Game1.cs
@@ -315,7 +315,7 @@ namespace PretzelPaladin
                             topRightMove.DrawWithText(_spriteBatch, Color.Red, subHeaderFont, rectangleTexture);
                             bottomLeftMove.DrawWithText(_spriteBatch, Color.Red, subHeaderFont, rectangleTexture);
                             bottomRightMove.DrawWithText(_spriteBatch, Color.Red, subHeaderFont, rectangleTexture);
-
+                            
                             
 
 
