02198f286fce529fe4a093cda04ac7b156f50c59
Storm Rivera <sxr7411@rit.edu>
2023-04-28 14:09:48 -0400

    GET IT

--- Group Project/PretzelPaladin/Game1.cs
+++ Group Project/PretzelPaladin/Game1.cs
@@ -216,10 +216,10 @@ namespace PretzelPaladin
             bap = this.Content.Load<Texture2D>("bap");
 
             // Enemy with Textures
-            enemy = new Enemy(sbarroSamuraiTexture, "Sbarro Samurai", 100, 100, 1, 1);
-            enemy3 = new Enemy(bap, "Biblically Accurate Pretzel", 100, 100, 1, 1);
-            enemy2 = new Enemy(bodok, "B.O.D.O.K", 110, 110, 1, 1);
-            player = new Player(rectangleTexture, "Pretzel Paladin", 500, 500, 1, 1);
+            enemy = new Enemy(sbarroSamuraiTexture, "Sbarro Samurai", 100, 1, 1);
+            enemy3 = new Enemy(bap, "Biblically Accurate Pretzel", 100, 1, 1);
+            enemy2 = new Enemy(bodok, "B.O.D.O.K", 110, 1, 1);
+            player = new Player(rectangleTexture, "Pretzel Paladin", 500, 1, 1);
             
             enemies.Add(enemy);
             enemies.Add(enemy2);
